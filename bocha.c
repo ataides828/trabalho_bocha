c219bb1ee6486a4407e50b67ca35189b1a18bcaa
ataides828 <32454706+ataides828@users.noreply.github.com>
2017-10-13 22:21:29 -0300

    Update bocha.c

--- bocha.c
+++ bocha.c
@@ -6,7 +6,6 @@
 #define TAM 10
 
 void gerar_matriz(char matriz[][TAM]) {
-
     int i,j;
     for(i = 0;i < TAM; i++ ) {
        for(j = 0;j < TAM; j++ ) {
@@ -59,12 +58,51 @@ void exibir(char matriz[][TAM]) {
 printf("\n --------------------------------------------");
 }
 
+int sorteio(){
+    int x;
+    srand((unsigned)time(NULL));
+
+    x = rand() % 10;
+    return x;
+}
+
+typedef struct {
+    char nome[51];
+    int jogadas, pontos;
+} jogador;
+
 int main() {
     char matriz[TAM][TAM];
+    int n_jogadores, inicia_jogo, i;
+    jogador jogadores[4];
+
+    setlocale(LC_ALL, "");
 
     gerar_matriz(matriz);
+    printf("\n          BEM VINDO AO BOCHA SIMULATOR\n\n");
     exibir(matriz);
 
+    printf("\n\n");
+    inicia_jogo = 0;
+    do {
+        printf("Informe o número de joadores\n");
+        scanf("%d", &n_jogadores);
 
+        if(n_jogadores == 2 || n_jogadores == 4) {
+            inicia_jogo = 1;
+        }
+    } while(inicia_jogo != 1);
+
+    for (i = 0; i < n_jogadores; i++) {
+        printf("Informe o nome do jogador %d\n", i+1);
+        scanf("%s", jogadores[i].nome);
+    }
+
+    // AQUI É JOGADO O BALIN
+    matriz[sorteio()][sorteio()] = 'X';
+    // LIMPA A TELA
+    system("cls");
+    printf("\n\n               BALIN JOGADO\n\n");
+    exibir(matriz);
     return 0;
 }
