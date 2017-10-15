8b10c18b97295fd21069a5a1f4529df05b321d91
ataides828 <32454706+ataides828@users.noreply.github.com>
2017-10-15 15:51:53 -0200

    Update bocha-teste-1.c

--- bocha-modificado/bocha-teste-1.c
+++ bocha-modificado/bocha-teste-1.c
@@ -5,8 +5,7 @@
 #include <ctype.h>
 #define TAM 10
 
-
-//Gerar a matriz
+//  Gerar a matriz
 void gerar_matriz(char matriz[][TAM]) {
     int i,j;
     for(i = 0;i < TAM; i++ ) {
@@ -16,51 +15,54 @@ void gerar_matriz(char matriz[][TAM]) {
     }
 }
 
-//Converter caractere em inteiro
+//  Converter caractere em inteiro
 int conv_char_int(char indice) {
     switch (indice)
     {
+        case 'A' :
         case 'a' :
             return 0;
         break;
+        case 'B' :
         case 'b' :
             return 1;
         break;
+        case 'C' :
         case 'c' :
             return 2;
         break;
+        case 'D' :
         case 'd' :
             return 3;
         break;
+        case 'E' :
         case 'e' :
             return 4;
         break;
+        case 'F' :
         case 'f' :
             return 5;
         break;
+        case 'G' :
         case 'g' :
             return 6;
         break;
+        case 'H' :
         case 'h' :
             return 7;
         break;
+        case 'I' :
         case 'i' :
             return 8;
         break;
+        case 'J' :
         case 'j' :
             return 9;
         break;
     }
 }
-char converter_char_int(char indice) {
-    char a, letra;
-    a = 'A';
-    letra = toupper(indice) - a;
 
-    return letra;
-}
-
-//Exibe a matriz
+//  Exibe a matriz
 void exibir(char matriz[][TAM]) {
     int i, j, x;
     int vetor_int[TAM];
@@ -68,7 +70,7 @@ void exibir(char matriz[][TAM]) {
 
     x = TAM;
 
-    for(i = 0 ;i < TAM;i++) {
+    for(i = 0; i < TAM; i++) {
         vetor_int[i] = x ;
         x--;
     }
@@ -94,52 +96,146 @@ void exibir(char matriz[][TAM]) {
         printf(" %c |", vetor_char[i]);
 
     }
-printf("\n --------------------------------------------");
+    printf("\n --------------------------------------------");
 }
 
-
-
-
-//Sorteia o balin
-int sorteio(){
-    int x;
+void sementeRand() {
     srand((unsigned)time(NULL));
+}
 
+//  Sorteia o balin
+int sorteio(){
+    int x, y;
     x = rand() % 10;
+
     return x;
 }
 
-//Limpa a tela
+//  Limpa a tela
 void limpa_tela() {
     system("cls");
 }
 
+// CALCULA COLUNA DA JOGADA
+int colunaErro(int coluna) {
+    int x, y;
+
+    if (coluna == 9) {
+        y = (rand() % 2) - 1;
+    } else if (coluna == 0) {
+        y = rand() % 2;
+    } else {
+        y = (rand() % 3) - 1;
+    }
+
+    x = coluna + y;
+    return x;
+}
+
+// CALCULA LINHA DA JOGADA
+int linhaErro(int linha) {
+    int x, y;
+
+    if (linha == 10) {
+        y = (rand() % 3) - 2;
+    } else if (linha == 9) {
+        y = (rand() % 4) - 2;
+    } else if (linha == 1) {
+        y = (rand() % 3);
+    } else if (linha == 2) {
+        y = (rand() % 4) - 1;
+    } else {
+        y = (rand() % 5) - 2;
+    }
+
+    x = linha + y;
+    return x;
+}
+
+//  STRUCT QUE ARMAZENA OS DADOS DE CADA JOGADOR
 typedef struct {
     char nome[51];
     int jogadas, pontos;
 } jogador;
 
+//  verifica qual jogador está fazendo a jogada
+int verifica_jogador(int jogada, int n_jogadores){
+    int j;
+    if (n_jogadores == 2) {
+        if ((jogada%2) == 0) {
+            j = 0;
+        } else {
+            j = 1;
+        }
+    } else {
+        switch(jogada){
+            case 0:
+            case 4:
+                j = 0;
+                break;
+            case 1:
+            case 5:
+                j = 1;
+                break;
+            case 2:
+            case 6:
+                j = 2;
+                break;
+            case 3:
+            case 7:
+                j = 3;
+                break;
+        }
+    }
+
+    return j;
+}
+
+// Pede para que o jogador faça uma jogada
+void solicita_jogada(char matriz[][TAM], int jogada, int n_jogadores, jogador jogadores[4]){
+    char coluna, letra, a;
+    int linha, j, linha_desviada, l, coluna_convertida, c;
+
+    j = verifica_jogador(jogada, n_jogadores);
+
+    printf("\n\n");
+    printf("%s em qual linha pretende jogar a bocha?  ", jogadores[j].nome);
+    scanf("%d", &linha);
+    printf("%s em qual coluna pretende jogar a bocha?  ", jogadores[j].nome);
+    scanf(" %c", &coluna);
+
+//  desvia a bocha da linha conforme a regra (-2, -1, 0, 1 e 2)
+    linha_desviada = linhaErro(linha);
+    l = 10 - linha_desviada;
+
+//  desvia a bocha da coluna conforme a regra (-1, 0 e 1)
+    coluna_convertida = conv_char_int(coluna);
+    c = colunaErro(coluna_convertida);
+
+    a = 'a';
+//  adiciona a letra corespondente ao jogador na matriz
+    letra = a + j;
+
+    matriz[l][c] = letra;
+}
 
 int main() {
-    char matriz[TAM][TAM];
-    int n_jogadores, inicia_jogo, i;
+    char matriz[TAM][TAM], coluna;
+    int n_jogadores, inicia_jogo, i, j, k, linha;
     jogador jogadores[4];
-    int linha;
-    char coluna;
 
     setlocale(LC_ALL, "");
+    sementeRand();
 
-    //In�cio do jogo
+//  Início do jogo
     gerar_matriz(matriz);
     printf("\n          BEM VINDO AO BOCHA SIMULATOR\n\n");
     exibir(matriz);
-
     printf("\n\n");
 
-
-    //Pede o n�mero de jogadores
+//  Pede o número de jogadores
     do {
-        printf("Informe o n�mero de jogadores [Escolha 2 ou 4] :  ");
+        printf("Informe o número de jogadores [Escolha 2 ou 4] :  ");
         scanf("%d", &n_jogadores);
         printf("\n");
 
@@ -148,39 +244,37 @@ int main() {
         }
     } while(inicia_jogo != 1);
 
-
-    //Informar nome do jogador
-    for (i = 0; i < n_jogadores ; i++) {
+//  Informar nome do jogador
+    for (i = 0; i < n_jogadores; i++) {
         printf("Informe o nome do jogador %d : ", i+1);
         scanf("%s", jogadores[i].nome);
+        jogadores[i].pontos = 0;
+        jogadores[i].jogadas = 0;
         printf("\n");
     }
 
-
-    // Joga-se o balin
+//  Joga-se o bolim
     matriz[sorteio()][sorteio()] = 'X';
     limpa_tela();
-
-
-
-    printf("\n\n--------->>> BALIN JOGADO COM SUCESSO\n\n");
+    printf("\n\n--------->>> BOLIM JOGADO COM SUCESSO\n\n");
     exibir(matriz);
 
+//  Solicita que sejam feitas as jogadas
+    for(i = 0; i < 8; i++) {
+        solicita_jogada(matriz, i, n_jogadores, jogadores);
+        j = verifica_jogador(i, n_jogadores);
+        // aumenta em um o numero de jogadas
+        jogadores[j].jogadas += 1;
 
-    printf("\n\n");
-    printf(" Em qual linha pretende jogar a bocha ?  ");
-    scanf("%d", &linha);
-    printf("Informe a coluna da matriz ?  ");
-    scanf(" %c", &coluna);
+        limpa_tela();
+        exibir(matriz);
+        printf("\n");
 
-
-    linha = 10 - linha;
-    printf("\n\n");
-    matriz[linha][conv_char_int(coluna)] = 'a';
-
-
-    exibir(matriz);
-
+        // exibe o numero de jogadas de cada jogador
+        for(k = 0; k < n_jogadores; k++){
+            printf("\nO jogador %s já fez %d jogadas", jogadores[k].nome, jogadores[k].jogadas);
+        }
+    }
 
     return 0;
 }
